a9cf7f7f56444ccda6e62c922cc5f05f80302098
thaipt <thaipt@dagoras.io>
2026-07-30 21:21:46 +0700

    Update check_env.py with python version

--- check_env.py
+++ check_env.py
@@ -1,5 +1,7 @@
+import sys
 import torch
 import sentence_transformers
 
+print(f"Python version: {sys.version}")
 print(f"PyTorch version: {torch.__version__}")
 print(f"Sentence-Transformers version: {sentence_transformers.__version__}")
